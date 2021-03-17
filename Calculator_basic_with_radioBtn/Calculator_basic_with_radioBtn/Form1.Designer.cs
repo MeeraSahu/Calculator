95e0b38255fb7ac7393993754a33b1f2f6cafad9
Meera <47111883+MeeraSahu@users.noreply.github.com>
2021-03-17 21:46:31 +0530

    first build
    calculator with radio button which can perform a basic operation such as addition subtraction multiplication and division

---
+++ Calculator_basic_with_radioBtn/Calculator_basic_with_radioBtn/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Calculator_basic_with_radioBtn
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Plus = new System.Windows.Forms.RadioButton();
+            this.Minus = new System.Windows.Forms.RadioButton();
+            this.Multi = new System.Windows.Forms.RadioButton();
+            this.Divide = new System.Windows.Forms.RadioButton();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ClearEntry = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(83, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 28);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(83, 47);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 28);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(83, 122);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 28);
+            this.textBox3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Data1";
+            // 
+            // Plus
+            // 
+            this.Plus.AutoSize = true;
+            this.Plus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Plus.Location = new System.Drawing.Point(258, 17);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(53, 19);
+            this.Plus.TabIndex = 3;
+            this.Plus.TabStop = true;
+            this.Plus.Text = "Plus";
+            this.Plus.UseVisualStyleBackColor = true;
+            // 
+            // Minus
+            // 
+            this.Minus.AutoSize = true;
+            this.Minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minus.Location = new System.Drawing.Point(258, 42);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(64, 19);
+            this.Minus.TabIndex = 3;
+            this.Minus.TabStop = true;
+            this.Minus.Text = "Minus";
+            this.Minus.UseVisualStyleBackColor = true;
+            // 
+            // Multi
+            // 
+            this.Multi.AutoSize = true;
+            this.Multi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Multi.Location = new System.Drawing.Point(258, 67);
+            this.Multi.Name = "Multi";
+            this.Multi.Size = new System.Drawing.Size(57, 19);
+            this.Multi.TabIndex = 3;
+            this.Multi.TabStop = true;
+            this.Multi.Text = "Multi";
+            this.Multi.UseVisualStyleBackColor = true;
+            // 
+            // Divide
+            // 
+            this.Divide.AutoSize = true;
+            this.Divide.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Divide.Location = new System.Drawing.Point(258, 92);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(65, 19);
+            this.Divide.TabIndex = 3;
+            this.Divide.TabStop = true;
+            this.Divide.Text = "Divide";
+            this.Divide.UseVisualStyleBackColor = true;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calculate.Location = new System.Drawing.Point(112, 81);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(98, 35);
+            this.Calculate.TabIndex = 4;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data3";
+            // 
+            // ClearEntry
+            // 
+            this.ClearEntry.BackColor = System.Drawing.Color.Red;
+            this.ClearEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearEntry.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClearEntry.Location = new System.Drawing.Point(258, 122);
+            this.ClearEntry.Name = "ClearEntry";
+            this.ClearEntry.Size = new System.Drawing.Size(65, 30);
+            this.ClearEntry.TabIndex = 5;
+            this.ClearEntry.Text = "CE";
+            this.ClearEntry.UseVisualStyleBackColor = false;
+            this.ClearEntry.Click += new System.EventHandler(this.ClearData);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(344, 164);
+            this.Controls.Add(this.ClearEntry);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Multi);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.Click += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton Plus;
+        private System.Windows.Forms.RadioButton Minus;
+        private System.Windows.Forms.RadioButton Multi;
+        private System.Windows.Forms.RadioButton Divide;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ClearEntry;
+    }
+}
+
---
+++ Calculator_basic_with_radioBtn/Calculator_basic_with_radioBtn/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_basic_with_radioBtn
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            Double data1 = Double.Parse(textBox1.Text);
+            Double data2 = Double.Parse(textBox2.Text);
+            Double data3 = 0;
+
+            if (Plus.Checked == true)
+                data3 = data1 + data2;
+            if (Minus.Checked == true)
+                data3 = data1 - data2;
+            if (Multi.Checked == true)
+                data3 = data1 * data2;
+            if (Divide.Checked == true)
+                data3 = data1 / data2;
+
+            textBox3.Text = Convert.ToString(data3);
+
+        }
+
+        private void ClearData(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+        }
+    }
+}
